dd0f76de313df19826e2eb6006f21f4fb4b1b28a
Anushka Mathur <76491255+asm2201@users.noreply.github.com>
2024-06-05 10:33:31 +0530

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anushka Mathur on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
